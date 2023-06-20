3f6f786be7b38523334adefa162bea32bdcedd14
Expertgraph <marcos.pierre@orange.fr>
2023-06-20 19:33:23 +0200

    b

--- Assets/0_Scripts/UI/MoucheCanvaToggle.cs
+++ Assets/0_Scripts/UI/MoucheCanvaToggle.cs
@@ -1,14 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MoucheCanvaToggle : MonoBehaviour
 {
-    /*
-    public void ShowHideCanves(Component sender, object data1, object data2, object data3)
+    public void OnFlyCanvaToggle(Component sender, object data1, object data2, object data3)
     {
-        if (d)
-
-        gameObject.SetActive((bool)data1);
-    }*/
+        if (data1 is not bool) return;
+        transform.GetChild(0).gameObject.SetActive((bool)data1);
+    }
 }
